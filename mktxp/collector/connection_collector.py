c47c6989cbaba308bc95d4eec5f461cadebe1b0e
ZingLix <me@zinglix.xyz>
2023-08-05 16:05:15 +0800

    support data rate per ip

--- mktxp/collector/connection_collector.py
+++ mktxp/collector/connection_collector.py
@@ -14,28 +14,67 @@
 
 from mktxp.collector.base_collector import BaseCollector
 from mktxp.flow.processor.output import BaseOutputProcessor
-from mktxp.datasource.connection_ds import IPConnectionDatasource, IPConnectionStatsDatasource
+from mktxp.datasource.connection_ds import (
+    IPConnectionDatasource,
+    IPConnectionStatsDatasource,
+    IPConnectionRateDatasource,
+)
 
 
 class IPConnectionCollector(BaseCollector):
-    ''' IP Connection Metrics collector
-    '''    
+    """ IP Connection Metrics collector
+    """
+
     @staticmethod
     def collect(router_entry):
-        if router_entry.config_entry.connections:       
+        if router_entry.config_entry.connections:
             connection_records = IPConnectionDatasource.metric_records(router_entry)
             if connection_records:
-                connection_metrics = BaseCollector.gauge_collector('ip_connections_total', 'Number of IP connections', connection_records, 'count',)
+                connection_metrics = BaseCollector.gauge_collector(
+                    "ip_connections_total",
+                    "Number of IP connections",
+                    connection_records,
+                    "count",
+                )
                 yield connection_metrics
 
         if router_entry.config_entry.connection_stats:
-            connection_stats_records = IPConnectionStatsDatasource.metric_records(router_entry)            
+            connection_stats_records = IPConnectionStatsDatasource.metric_records(
+                router_entry
+            )
 
             for connection_stat_record in connection_stats_records:
-                BaseOutputProcessor.augment_record(router_entry, connection_stat_record, id_key = 'src_address')
+                BaseOutputProcessor.augment_record(
+                    router_entry, connection_stat_record, id_key="src_address"
+                )
 
-            connection_stats_labels = ['src_address', 'dst_addresses', 'dhcp_name']                
-            connection_stats_metrics_gauge = BaseCollector.gauge_collector('connection_stats', 'Open connection stats', 
-                                                                connection_stats_records, 'connection_count', connection_stats_labels)
+            connection_stats_labels = ["src_address", "dst_addresses", "dhcp_name"]
+            connection_stats_metrics_gauge = BaseCollector.gauge_collector(
+                "connection_stats",
+                "Open connection stats",
+                connection_stats_records,
+                "connection_count",
+                connection_stats_labels,
+            )
+            connection_rate_records = IPConnectionRateDatasource.metric_records(
+                router_entry
+            )
+            for connection_rate in connection_rate_records:
+                BaseOutputProcessor.augment_record(
+                    router_entry, connection_rate, id_key="src_address"
+                )
+            connection_rates_labels = [
+                "src_address",
+                "dst_addresses",
+                "orig_rate",
+                "reply_rate",
+            ]
+            connection_rates_metrics_gauge = BaseCollector.gauge_collector(
+                "connection_rate",
+                "Open connection rates",
+                connection_rates_metrics_gauge,
+                "connection_rate",
+                connection_rates_labels,
+            )
             yield connection_stats_metrics_gauge
 
--- mktxp/datasource/connection_ds.py
+++ mktxp/datasource/connection_ds.py
@@ -17,40 +17,53 @@ from mktxp.datasource.base_ds import BaseDSProcessor
 
 
 class IPConnectionDatasource:
-    ''' IP connections data provider
-    '''             
+    """ IP connections data provider
+    """
+
     @staticmethod
-    def metric_records(router_entry, *, metric_labels = None):
+    def metric_records(router_entry, *, metric_labels=None):
         if metric_labels is None:
-            metric_labels = []        
+            metric_labels = []
         try:
-            answer = router_entry.api_connection.router_api().get_binary_resource('/ip/firewall/connection/').call('print', {'count-only': b''})
+            answer = (
+                router_entry.api_connection.router_api()
+                .get_binary_resource("/ip/firewall/connection/")
+                .call("print", {"count-only": b""})
+            )
             # answer looks and feels like an empty list: [], but it has a special attribute `done_message`
             done_message = answer.done_message
             # `done_msg` is a dict with the return code as a key - which is the count that we are looking for
-            cnt = done_message['ret'].decode()
-            records = [{'count': cnt}]
-            return BaseDSProcessor.trimmed_records(router_entry, router_records = records, metric_labels = metric_labels)
+            cnt = done_message["ret"].decode()
+            records = [{"count": cnt}]
+            return BaseDSProcessor.trimmed_records(
+                router_entry, router_records=records, metric_labels=metric_labels
+            )
         except Exception as exc:
-            print(f'Error getting IP connection info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}')
+            print(
+                f"Error getting IP connection info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}"
+            )
             return None
 
 
 class IPConnectionStatsDatasource:
-    ''' IP connections stats data provider
-    '''             
+    """ IP connections stats data provider
+    """
+
     @staticmethod
-    def metric_records(router_entry, *, metric_labels = None, add_router_id = True):
+    def metric_records(router_entry, *, metric_labels=None, add_router_id=True):
         if metric_labels is None:
-            metric_labels = []        
+            metric_labels = []
         try:
-            connection_records = router_entry.api_connection.router_api().get_resource('/ip/firewall/connection/').call('print', \
-                                                                                            {'proplist':'src-address,dst-address,protocol'})
-             # calculate number of connections per src-address
+            connection_records = (
+                router_entry.api_connection.router_api()
+                .get_resource("/ip/firewall/connection/")
+                .call("print", {"proplist": "src-address,dst-address,protocol"})
+            )
+            # calculate number of connections per src-address
             connections_per_src_address = {}
             for connection_record in connection_records:
-                #address, port = (connection_record['src-address'].split(':') + [None])[:2]
-                address = connection_record['src-address'].split(':')[0]
+                # address, port = (connection_record['src-address'].split(':') + [None])[:2]
+                address = connection_record["src-address"].split(":")[0]
                 destination = f"{connection_record.get('dst-address')}({connection_record.get('protocol')})"
 
                 count, destinations = 0, set()
@@ -58,20 +71,66 @@ class IPConnectionStatsDatasource:
                     count, destinations = connections_per_src_address[address]
                 count += 1
                 destinations.add(destination)
-                connections_per_src_address[address] = ConnStatsEntry(count, destinations)
+                connections_per_src_address[address] = ConnStatsEntry(
+                    count, destinations
+                )
 
             # compile connections-per-interface records
             records = []
             for key, entry in connections_per_src_address.items():
-                record = {'src_address': key, 'connection_count': entry.count, 'dst_addresses': ', '.join(entry.destinations)}
-                if add_router_id: 
+                record = {
+                    "src_address": key,
+                    "connection_count": entry.count,
+                    "dst_addresses": ", ".join(entry.destinations),
+                }
+                if add_router_id:
                     for router_key, router_value in router_entry.router_id.items():
                         record[router_key] = router_value
                 records.append(record)
-            return records 
+            return records
         except Exception as exc:
-            print(f'Error getting IP connection stats info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}')
+            print(
+                f"Error getting IP connection stats info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}"
+            )
             return None
 
 
-ConnStatsEntry = namedtuple('ConnStatsEntry', ['count', 'destinations'])
+class IPConnectionRateDatasource:
+    """ IP connections stats data provider
+    """
+
+    @staticmethod
+    def metric_records(router_entry, *, metric_labels=None, add_router_id=True):
+        if metric_labels is None:
+            metric_labels = []
+        try:
+            connection_records = (
+                router_entry.api_connection.router_api()
+                .get_resource("/ip/firewall/connection/")
+                .call(
+                    "print",
+                    {
+                        "proplist": "src-address,dst-address,orig-rate,repl-rate,orig-bytes,repl-bytes,protocol"
+                    },
+                )
+            )
+
+            # compile connections-per-interface records
+            records = []
+            for item in connection_records:
+                record = item
+                item["src-address"] = item["src-address"].split(":")[0]
+                if add_router_id:
+                    for router_key, router_value in router_entry.router_id.items():
+                        record[router_key] = router_value
+                records.append(record)
+            return records
+        except Exception as exc:
+            print(
+                f"Error getting IP connection stats info from router{router_entry.router_name}@{router_entry.config_entry.hostname}: {exc}"
+            )
+            return None
+
+
+ConnStatsEntry = namedtuple("ConnStatsEntry", ["count", "destinations"])
+
